f04bb58b1349dfd9f9836f63a36c7e886f50f774
NoelsCodes <tankaiwei@live.com>
2024-04-20 11:15:34 +0800

    dropbox

--- src/components/Dropbox.js
+++ src/components/Dropbox.js
@@ -13,6 +13,18 @@ function dropbox() {
 }
 export default dropbox;
 
+
+// "month": "2017-12",  <= range
+// "town": "SEMBAWANG", <= list selection
+// "flat_type": "4 ROOM", <= list selection
+// "storey_range": "07 TO 09", <= list selection
+// "floor_area_sqm": "85", <= range
+// "flat_model": "Model A", <= list selection
+// "remaining_lease": "85 years 08 months", list selection
+// "resale_price": "355000" <= range
+
+
+
 // // These countries need to be displayed as a dropdown in the input field
 // const countries = ["Canada", "Russia", "Germany", "Italy"];
 
@@ -34,3 +46,39 @@ export default dropbox;
 // <input type="text" list="countriesDataList" id="countryInput" />
 
 // <datalist id="countriesDataList"></datalist>
+
+
+// ANG MO KIO
+// BEDOK
+// BISHAN
+// BUKIT BATOK
+// BUKIT MERAH
+// BUKIT PANJANG
+// BUKIT TIMAH
+// CENTRAL AREA
+// CHOA CHU KANG
+// CLEMENTI
+// GEYLANG
+// HOUGANG
+// JURONG EAST
+// JURONG WEST
+// KALLANG/WHAMPOA
+// MARINE PARADE
+// PASIR RIS
+// PUNGGOL
+// QUEENSTOWN
+// SEMBAWANG
+// SENGKANG
+// SERANGOON
+// TAMPINES
+// TOA PAYOH
+// WOODLANDS
+// YISHUN
+
+// 2 ROOM
+// 3 ROOM
+// 4 ROOM
+// 5 ROOM
+// EXECUTIVE
+// 1 ROOM
+// MULTI-GENERATION
