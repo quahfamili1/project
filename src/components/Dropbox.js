49de048e3c756396f031db20864f66b3fb54a0f0
NoelsCodes <tankaiwei@live.com>
2024-04-21 11:17:12 +0800

    dropbox wip

--- src/components/Dropbox.js
+++ src/components/Dropbox.js
@@ -1,17 +1,71 @@
-const list = ["AMK", "CCK", "BG", "BW"];
+function Dropbox(){
+  
+  const FlatType = [
+    '2 ROOM',
+    '3 ROOM',
+    '4 ROOM',
+    '5 ROOM',
+    'EXECUTIVE',
+    '1 ROOM',
+    'MULTI-GENERATION'
+    ]
 
-function dropbox() {
   return (
-    <input list="Town" name="browser" id="browser">
-    <datalist id="country">
-      <option value="AMK"/>
-      <option value="CCK"/>
-      <option value="BG"/>
-    </datalist>
-    </input>
-  );
-}
-export default dropbox;
+  <>
+  <select>
+      {FlatType.map (=> { return <option value="volvo">Volvo</option>;})
+      }
+  </select>
+  <select>
+      <option value="volvo">Volvo</option>
+      <option value="saab">Saab</option>
+  </select><br></br>
+  <select>
+      <option value="volvo">Volvo</option>
+      <option value="saab">Saab</option>
+  </select>
+  <select>
+      <option value="volvo">Volvo</option>
+      <option value="saab">Saab</option>
+  </select><br></br>
+  <select>
+      <option value="volvo">Volvo</option>
+      <option value="saab">Saab</option>
+  </select>
+  <select>
+      <option value="volvo">Volvo</option>
+      <option value="saab">Saab</option>
+  </select><br></br>
+  <select>
+      <option value="volvo">Volvo</option>
+      <option value="saab">Saab</option>
+  </select>
+  <select>
+      <option value="volvo">Volvo</option>
+      <option value="saab">Saab</option>
+  </select><br></br>
+  <button>Search
+  </button>
+  </>);
+  }
+  export default Dropbox;
+
+
+
+// const list = ["AMK", "CCK", "BG", "BW"];
+
+// function dropbox() {
+//   return (
+//     <input list="Town" name="browser" id="browser">
+//     <datalist id="country">
+//       <option value="AMK"/>
+//       <option value="CCK"/>
+//       <option value="BG"/>
+//     </datalist>
+//     </input>
+//   );
+// }
+// export default dropbox;
 
 
 // "month": "2017-12",  <= range
@@ -23,7 +77,7 @@ export default dropbox;
 // "remaining_lease": "85 years 08 months", list selection
 // "resale_price": "355000" <= range
 
-
+// 11.18
 
 // // These countries need to be displayed as a dropdown in the input field
 // const countries = ["Canada", "Russia", "Germany", "Italy"];
@@ -47,6 +101,7 @@ export default dropbox;
 
 // <datalist id="countriesDataList"></datalist>
 
+//Town
 
 // ANG MO KIO
 // BEDOK
@@ -75,10 +130,46 @@ export default dropbox;
 // WOODLANDS
 // YISHUN
 
-// 2 ROOM
-// 3 ROOM
-// 4 ROOM
-// 5 ROOM
-// EXECUTIVE
-// 1 ROOM
-// MULTI-GENERATION
+
+
+//Story Range
+// 10 TO 12
+// 01 TO 03
+// 04 TO 06
+// 07 TO 09
+// 13 TO 15
+// 19 TO 21
+// 22 TO 24
+// 16 TO 18
+// 34 TO 36
+// 28 TO 30
+// 37 TO 39
+// 49 TO 51
+// 25 TO 27
+// 40 TO 42
+// 31 TO 33
+// 46 TO 48
+// 43 TO 45
+
+//Flat Model
+// Improved
+// New Generation
+// DBSS
+// Standard
+// Apartment
+// Simplified
+// Model A
+// Premium Apartment
+// Adjoined flat
+// Model A-Maisonette
+// Maisonette
+// Type S1
+// Type S2
+// Model A2
+// Terrace
+// Improved-Maisonette
+// Premium Maisonette
+// Multi Generation
+// Premium Apartment Loft
+// 2-room
+// 3Gen
--- src/routes/Result.js
+++ src/routes/Result.js
@@ -1,8 +1,7 @@
-import dropbox from "../components/Dropbox";
+
 
 function Result() {
   return <><div>This is the result page</div>
-  <dropbox/>
   </>;
 }
 export default Result;
