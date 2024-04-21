a14aea16a912db64cd561975dfc7959c7a25f18e
quahfamili1 <quahfamili@live.com.sg>
2024-04-21 12:15:54 +0800

    added about page and add img asset

--- src/routes/Aboutus.js
+++ src/routes/Aboutus.js
@@ -1,4 +1,21 @@
-function Aboutus() {
-  return <div>This is the About us page</div>;
-}
+import dev from "../assets/dev.jpg";
+import styles from "./Aboutus.module.css";
+
+const Aboutus = () => {
+  const teamName = "Awesome Developers";
+  const teamDescription =
+    "We are a group of passionate students learning full-stack development.";
+
+  const inspiringNote = "Together, we build dreams into code!";
+
+  return (
+    <div className={styles.aboutuscontainer}>
+      <h1 className={styles.teamname}>{teamName}</h1>
+      <img className={styles.teamimage} src={dev} alt="Team" />
+      <p className={styles.teamdescription}>{teamDescription}</p>
+      <p className={styles.inspiringnote}>{inspiringNote}</p>
+    </div>
+  );
+};
+
 export default Aboutus;
